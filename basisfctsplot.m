74211c2fb2dd8245d660a9f3fd968849858ee2a0
Kipfstuhl Jonas <jonas.kipfstuhl@t-online.de>
2017-03-04 17:29:28 +0100

    basisfunctions for tensor product construction

---
+++ basisfctsplot.m
@@ -0,0 +1,37 @@
+%% Plot basis functions for product spaces
+%  polynomial of deg N+1 in x firection and N in y direction
+
+N = 4;
+
+[xnodes,~,~] = lglnodes(N);
+[ynodes,~] = lgwt(N, -1, 1);           % returns also the
+                                       % Vandermonde matrix
+
+f = zeros(N,N+1);
+for i=1:N
+    fvals = zeros(N+1, 1);
+    fvals(i) = 1.0;
+    f(i,:) = polyfit(xnodes, fvals, N);
+end
+
+g = zeros(N-1, N);
+for i=1:N-1
+    gvals = zeros(N, 1);
+    gvals(i) = 1.0;
+    g(i,:) = polyfit(ynodes, gvals, N-1);
+end
+
+% make product functions f(i)*g(i)
+h = cell(N, N-1);
+[xx, yy] = meshgrid(linspace(-1, 1, 100), linspace(-1, 1, 100));
+%zz = zeros(N, N-1, 100, 100);
+zz = cell(N, N-1);
+for i=1:N
+    for j=1:N-1
+        h_temp = @(x, y) polyval(f(i,:), x) * polyval(g(j,:), y);
+        h{i, j} = h_temp;
+        zz{i, j} = h_temp(xx, yy);
+    end
+end
+
+
